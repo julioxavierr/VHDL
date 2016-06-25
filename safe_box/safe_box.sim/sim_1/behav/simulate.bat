e5c8ada48d8916939dbde52e2928df27e8a199b6
Julio Xavier <julioxavierr@live.com>
2016-06-25 01:13:25 -0300

    Mux8x4

--- safe_box/safe_box.sim/sim_1/behav/simulate.bat
+++ safe_box/safe_box.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.1\\bin
-call %xv_path%/xsim mux2x1_behav -key {Behavioral:sim_1:Functional:mux2x1} -tclbatch mux2x1.tcl -log simulate.log
+call %xv_path%/xsim mux8x1_behav -key {Behavioral:sim_1:Functional:mux8x1} -tclbatch mux8x1.tcl -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
---
+++ safe_box/safe_box.srcs/sources_1/new/mux8x1.vhd
@@ -0,0 +1,54 @@
+-- Mux 8x4 utilizando quatro mux2x1
+-- Entrada A_n, B_n Saída Y_n
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux8x1 is
+Port (	
+        SEL : in STD_LOGIC;
+        A_1 : in STD_LOGIC;
+		B_1 : in STD_LOGIC;
+		A_2 : in STD_LOGIC;
+		B_2 : in STD_LOGIC;
+		A_3 : in STD_LOGIC;
+		B_3 : in STD_LOGIC;
+		A_4 : in STD_LOGIC;
+		B_4 : in STD_LOGIC;
+		A_5 : in STD_LOGIC;
+		B_5 : in STD_LOGIC;
+		Y_1 : out STD_LOGIC;
+		Y_2 : out STD_LOGIC;
+		Y_3 : out STD_LOGIC;
+		Y_4 : out STD_LOGIC
+	);
+end mux8x1;
+
+architecture Behavioral of mux8x1 is
+
+component mux2x1 is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           SEL : in STD_LOGIC;
+           R : out STD_LOGIC);
+end component;
+
+
+
+begin
+
+MUX1: mux2x1 port map (A => A_1, B => B_1, SEL => SEL, R => Y_1);
+MUX2: mux2x1 port map (A => A_2, B => B_2, SEL => SEL, R => Y_2);
+MUX3: mux2x1 port map (A => A_3, B => B_3, SEL => SEL, R => Y_3);
+MUX4: mux2x1 port map (A => A_4, B => B_4, SEL => SEL, R => Y_4);
+
+end Behavioral;
